cfafe2a37ec865bc1889dfb8c408ed97e5c642d5
Matthias Grandl <matthias.grandl@croit.io>
2024-03-17 08:48:14 +0100

    use GPUI svg rendering and cache favicons in memory

--- src/components/shared/mod.rs
+++ src/components/shared/mod.rs
@@ -9,24 +9,28 @@
  *
  */
 
-use std::{cell::OnceCell, path::PathBuf, sync::Arc};
+use std::{
+    cell::OnceCell,
+    collections::HashMap,
+    path::PathBuf,
+    sync::{Arc, OnceLock},
+};
 
 use anyhow::anyhow;
-use async_std::{
-    fs,
-    task::{spawn, spawn_blocking},
-};
+use async_std::task::{spawn, spawn_blocking, JoinHandle};
+use futures::future::Shared;
+use futures::FutureExt;
 use gpui::*;
-use image::ImageFormat;
-use log::debug;
+use log::{debug, error};
+use parking_lot::Mutex;
+use scraper::{Html, Selector};
 use url::Url;
 
-use crate::{paths::paths, theme::Theme};
-use scraper::{Html, Selector};
+pub use icon::Icon;
+
+use crate::theme::Theme;
 
 mod icon;
-
-pub use icon::Icon;
 
 #[derive(Clone)]
 #[allow(dead_code)]
@@ -123,11 +127,7 @@ impl RenderOnce for Img {
             return favicon.clone().into_any_element();
         }
         let theme = cx.global::<Theme>();
-        let el = div()
-            .overflow_hidden()
-            .flex()
-            .items_center()
-            .justify_center();
+        let el = div().flex().items_center().justify_center();
         let el = match self.mask {
             ImgMask::Circle => el.rounded_full().bg(theme.surface0),
             ImgMask::Rounded => el.rounded_md().bg(theme.surface0),
@@ -154,7 +154,7 @@ impl RenderOnce for Img {
                 svg.into_any_element()
             }
             ImgSource::Base(src) => {
-                let img = img(src).size_full();
+                let img = img(src).object_fit(ObjectFit::Cover).size_full();
                 let img = match self.mask {
                     ImgMask::Circle => {
                         el = el.p_0p5();
@@ -178,25 +178,33 @@ impl RenderOnce for Img {
 }
 
 pub struct NoView;
+
 impl Render for NoView {
     fn render(&mut self, _cx: &mut ViewContext<Self>) -> impl IntoElement {
         div()
     }
 }
 
+type FetchFaviconTask = Shared<JoinHandle<Result<SharedUri, Arc<anyhow::Error>>>>;
+
+static FAVICONS: OnceLock<Arc<Mutex<HashMap<String, FetchFaviconTask>>>> = OnceLock::new();
+
 #[derive(Clone)]
 pub struct Favicon {
     img: Img,
-    url: String,
     fallback: Icon,
-    path: Option<PathBuf>,
-    init: OnceCell<bool>,
+    url: String,
+    task: OnceCell<FetchFaviconTask>,
 }
 
 impl Favicon {
-    async fn fetch_favicon(url: Url, cache: PathBuf) -> anyhow::Result<PathBuf> {
+    async fn find_favicon(url: impl ToString) -> Result<SharedUri, anyhow::Error> {
+        let url = Url::parse(&url.to_string())?;
+        if url.cannot_be_a_base() || !url.scheme().starts_with("http") {
+            return Err(anyhow!("Invalid URL"));
+        };
         let base_url = Url::parse(&format!("{}://{}", url.scheme(), url.host_str().unwrap()))?;
-        let mut targets = vec![url.clone(), base_url.clone()];
+        let mut targets = vec![base_url.clone()];
         // if subdomain
         if let Some(domain) = base_url.domain() {
             let split: Vec<&str> = domain.split('.').collect();
@@ -209,12 +217,16 @@ impl Favicon {
             }
         };
         let client = reqwest::ClientBuilder::new()
-            .user_agent("favicon_crawler (loungy.app)")
+            .user_agent("http_client (loungy.app)")
             .build()?;
         for target in targets {
-            let response = client.get(target.clone()).send().await?;
+            let Ok(response) = client.get(target.clone()).send().await else {
+                continue;
+            };
             let url = response.url().clone();
-            let html = response.text().await?;
+            let Ok(html) = response.text().await else {
+                continue;
+            };
 
             let mut hrefs: Vec<String> = spawn_blocking(move || {
                 let document = Html::parse_document(&html);
@@ -230,51 +242,12 @@ impl Favicon {
 
             for href in hrefs {
                 let absolute = Url::parse(&href).unwrap_or(url.join(&href).unwrap());
-                let Ok(response) = client.get(absolute).send().await else {
+                let Ok(response) = client.get(absolute.to_string()).send().await else {
                     continue;
                 };
-                let Ok(bytes) = response.bytes().await else {
-                    continue;
+                if response.status().is_success() {
+                    return Ok(absolute.to_string().into());
                 };
-
-                if let Ok(format) = image::guess_format(&bytes) {
-                    if format == ImageFormat::Png {
-                        let _ = fs::write(&cache, &bytes).await;
-                    } else {
-                        let Ok(img) = image::load_from_memory_with_format(&bytes, format) else {
-                            continue;
-                        };
-                        let _ = img.save_with_format(&cache, ImageFormat::Png);
-                    }
-                    if let Ok(dimensions) = image::image_dimensions(&cache) {
-                        if dimensions.0 > 50 {
-                            return Ok(cache);
-                        }
-                    }
-                } else {
-                    let tree = resvg::usvg::Tree::from_data(
-                        &bytes,
-                        &resvg::usvg::Options::default(),
-                        &resvg::usvg::fontdb::Database::default(),
-                    )?;
-                    let size = tree.size();
-                    let width = 64;
-                    let height = 64;
-                    let mut pixmap = resvg::tiny_skia::Pixmap::new(width, height)
-                        .expect("failed to create pixmap");
-                    let mut pixmap_mut = pixmap.as_mut();
-                    resvg::render(
-                        &tree,
-                        resvg::usvg::Transform::from_scale(
-                            width as f32 / size.width(),
-                            height as f32 / size.height(),
-                        ),
-                        &mut pixmap_mut,
-                    );
-                    if pixmap.save_png(&cache).is_ok() {
-                        return Ok(cache);
-                    }
-                }
             }
         }
 
@@ -286,60 +259,43 @@ impl Favicon {
         fallback: Icon,
         cx: &mut WindowContext,
     ) -> View<Self> {
-        cx.new_view(|_| Self {
+        let url = url.to_string();
+        cx.new_view(|_cx| Self {
             img: img.clone(),
-            url: url.to_string(),
             fallback,
-            path: None,
-            init: OnceCell::new(),
+            url: url.to_string(),
+            task: OnceCell::new(),
         })
-    }
-    pub fn init(&mut self, cx: &mut ViewContext<Self>) {
-        self.init.get_or_init(|| {
-            let Ok(url) = Url::parse(&self.url) else {
-                return true;
-            };
-            if url.cannot_be_a_base() || !url.scheme().starts_with("http") {
-                return true;
-            };
-
-            let Some(host) = url.host_str() else {
-                return true;
-            };
-
-            let cache = paths().cache.join("favicons").join(format!("{}.png", host));
-
-            if let Ok(exists) = cache.try_exists() {
-                if exists {
-                    self.path = Some(cache);
-                    cx.notify();
-                    return true;
-                }
-            }
-
-            cx.spawn(|view, mut cx| async move {
-                // TODO: For some reason, if lots of favicons are requested at once, only the first x will be fetched.
-                let result = spawn(Self::fetch_favicon(url, cache)).await;
-                if let Ok(path) = result {
-                    let _ = view.update(&mut cx, |this, cx| {
-                        this.path = Some(path);
-                        cx.notify();
-                    });
-                } else {
-                    debug!("Failed to fetch favicon: {:?}", result.err());
-                }
-            })
-            .detach();
-            true
-        });
     }
 }
 
 impl Render for Favicon {
-    fn render(&mut self, cx: &mut ViewContext<Self>) -> impl IntoElement {
-        self.init(cx);
-        if let Some(path) = self.path.clone() {
-            self.img.clone().file(path)
+    fn render(&mut self, _cx: &mut ViewContext<Self>) -> impl IntoElement {
+        if let Some(task) = self
+            .task
+            .get_or_init(|| {
+                FAVICONS
+                    .get_or_init(Default::default)
+                    .lock()
+                    .entry(self.url.clone())
+                    .or_insert_with(|| {
+                        let url = self.url.clone();
+                        spawn(async move {
+                            Self::find_favicon(url).await.map_err(|err| {
+                                let error = Arc::new(err);
+                                debug!("{}", error);
+                                error
+                            })
+                        })
+                        .shared()
+                    })
+                    .clone()
+            })
+            .clone()
+            .now_or_never()
+            .and_then(|result| result.ok())
+        {
+            self.img.clone().url(task)
         } else {
             self.img.clone().icon(self.fallback.clone())
         }
